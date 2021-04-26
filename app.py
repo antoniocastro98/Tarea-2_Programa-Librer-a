f13855f7e7513cb67ffa9f10e726d7bc9b7188c3
root <antoniocastro-@hotmail.com>
2021-04-27 01:09:06 +0200

    añadida función de la pagina del libro

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ with open("books.json") as archivo:
 def inicio():
     return render_template("inicio.html",libros=datos)
 
+
+@app.route('/libro/<isbn>')
+def libro(isbn):
+    for i in datos:
+        if "isbn" in i.keys() and isbn == i["isbn"]:
+            return render_template("libros.html",libro=i)
+    abort(404)
+
 port=os.environ["PORT"]
 app.run('0.0.0.0', int(port), debug=False)
